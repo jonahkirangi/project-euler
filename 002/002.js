8408a69b4b6a006497f661319dac438b8e95a8b5
Jonah Kirangi <jonahkirangi@gmail.com>
2014-09-14 23:11:19 -0700

    Solution to problem 2

--- 002/002.js
+++ 002/002.js
@@ -1,17 +1,17 @@
 // Create fibonacci array beginning with the first two elements
 var fib = [1, 2];
-var sum = 0;
+var evenSum = 0;
 
-// Add new elements to fibonacci sequence
 // Ensure that the final fibonacci element does not exceed 4 million
 for (i = 0; fib[fib.length - 1] < 4000000; i++) {
-  fib.push((fib[fib.length - 1]) + (fib[fib.length - 2]));
 
-
-  // Find the sum of the even valued fibonacci elements
+  // Tally the sum of the even-valued fibonacci elements
   if ((fib[fib.length - 1]) % 2 === 0) {
-    sum += fib[fib.length - 1];
+    evenSum += fib[fib.length - 1];
   }
+
+  // Push new elements to fibonacci sequence
+  fib.push((fib[fib.length - 1]) + (fib[fib.length - 2]));
 }
 
-console.log(fib);
+console.log(evenSum);
